78b0cf13512670b03fb9833c1774e87879b3963a
Gostev, Philipp <philipp.gostev@adesso.de>
2023-02-20 13:00:38 +0100

    Release 2.0 (unfinished)
    - Cart Status
    - Add to cart from
      -> Product-List
      -> Product-Details
    - Cart Details
    - Increment/Decrement Items
    - Remove Items from Shopping Cart

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Product} from "../../common/product";
 import {ProductService} from "../../services/product.service";
 import {ActivatedRoute} from "@angular/router";
+import {CartItem} from "../../common/cart-item";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-product-details',
@@ -12,6 +14,7 @@ export class ProductDetailsComponent implements OnInit{
 
   product!: Product;
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute) {
   }
   ngOnInit(): void {
@@ -27,5 +30,13 @@ export class ProductDetailsComponent implements OnInit{
 
     this.productService.getProduct(theProductId).subscribe(
       data => this.product = data);
+  }
+
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+
+    const theCartItem = new CartItem(this.product);
+
+    this.cartService.addToCart(theCartItem);
   }
 }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
 import {ActivatedRoute} from "@angular/router";
+import {CartItem} from "../../common/cart-item";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-product-list',
@@ -24,7 +26,9 @@ export class ProductListComponent implements OnInit{
   previousKeyWord: string = "";
 
   constructor(private productService: ProductService,
-              private route: ActivatedRoute) {
+              private cartService: CartService,
+              private route: ActivatedRoute
+              ) {
   }
   ngOnInit(): void{
     this.route.paramMap.subscribe(() => {
@@ -101,5 +105,13 @@ export class ProductListComponent implements OnInit{
       this.thePageSize = data.page.size;
       this.theTotalElements = data.page.totalElements;
     };
+  }
+
+  addToCart(theProduct: Product) {
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
   }
 }
